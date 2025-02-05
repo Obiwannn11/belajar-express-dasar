6934ec9ca572f905238e5886844a418ae2cccf47
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-06 02:55:32 +0800

    membuat basic login controller yaitu validasi data yang masuk dengan zod, lalu cari email, dan ubah input password menjadi bcrypt untuk di bandingan yang di db

---
+++ controller/loginController.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const loginSchema = require('../validation/loginValidation');
+
+class LoginController {
+    constructor(model) {
+        this._model = model;
+    }
+
+    login = catchAsync(async (req, res, next) => {
+        // Validasi input dengan Zod
+        const validatedData = loginSchema.safeParse(req.body);
+        if (!validatedData.success) {
+            return next(new AppError(validatedData.error.errors[0].message, 400));
+        }
+
+        const { email, password } = validatedData.data;
+
+        // Cek apakah email ada di database
+        const user = await this._model.findOne({ where: { email } });
+        if (!user) {
+            return next(new AppError('Email tidak terdaftar', 400));
+        }
+
+        // Periksa kecocokan password
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return next(new AppError('Password salah', 400));
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: 'Login berhasil',
+            data: {
+                id: user.id,
+                fullname: user.fullname,
+                email: user.email,
+                phone: user.phone,
+                status: user.status,
+            },
+        });
+    });
+}
+
+module.exports = LoginController;
