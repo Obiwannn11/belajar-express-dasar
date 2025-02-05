5247fe6bd89be900543e99ab70a66fea4493baa7
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-06 02:06:50 +0800

    memperbaiki nama Objek controller dari Index menjadi Signup

--- routes/penggunaRoute.js
+++ routes/penggunaRoute.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 // indexcontroller hanyalah sebuah controller untuk menangani crud simple, yaitu Create,Read,Update,Delete
-const IndexController = require('../controller/indexController.js')
+const SignupController = require('../controller/penggunaController.js')
 
 const Signup = (model) => {
     const router = express.Router()
-    const Signup = new IndexController(model)
+    const Signup = new SignupController(model)
 
     router.get('/', Signup.index)
     // router.get('/:id', Index.show)
