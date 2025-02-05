db91c235467315ba748a065bc08e6a557b4f71f6
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-05 19:22:33 +0800

    menambahkan pesan error dan memindahkan router

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const env = dotenv.config().parsed;
+
+//orm sequelize
+const { sequelize } = require('./config/database');
+const { QueryTypes } = require('sequelize');
+
+// route
+const routes = require('./routes')
+
+//error handler
+const catchAsync = require('./utils/catchAsync');
+const AppError = require('./utils/appError');
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -9,6 +21,25 @@ app.listen(env.PORT, () => {
     console.log(`Server Berjalan di Port ${env.PORT}`)
 })
 
+
+// Route
+app.use(routes);
+// app.use('/', routes);
+
+
+//error handler sementara 
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
+
+//cek cek apakah api berjalan normal
 app.get('/', (req, res) => {
     try {
         return res.status(200).json({
@@ -20,6 +51,24 @@ app.get('/', (req, res) => {
         return res.status(500).json({
             status: "Gagal",
             message: "Server Gagal Koneksi"
+        })
+        
+    }
+})
+
+//testing postgres
+app.get('/postgres', async(req, res) => {
+    try {
+        const result = await sequelize.query('SELECT * FROM "Users" ', {
+            type: QueryTypes.SELECT,
+          });
+          res.status(200).json({ message: 'Koneksi Sequelize Berhasil', data: result });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            status: "Gagal",
+            message: "Koneksi Gagal",
+            error: err.message,
         })
         
     }
