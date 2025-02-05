4217de9506a5fd4e604755921096b563e1f8d944
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-06 02:06:18 +0800

    memperbaiki nama Objek controller dari Index menjadi Signup

--- controller/penggunaController.js
+++ controller/penggunaController.js
@@ -1,16 +1,47 @@
-class IndexController {
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+class SignupController {
     _model;
 
     constructor(model) {
         this._model = model
     }
 
+    store = catchAsync(async (req, res, next) => {
+        const { fullname, email, password } = req.body;
+
+        // Validasi input
+        if (!fullname || !email || !password) {
+            return next(new AppError('Semua kolom (name, email, password) wajib diisi', 400));
+        }
+
+        // Cek apakah email sudah digunakan
+        const existingUser = await this._model.findOne({ where: { email } });
+        if (existingUser) {
+            return next(new AppError('Email sudah terdaftar, gunakan email lain', 400));
+        }
+
+        // Simpan data
+        const data = await this._model.create(req.body);
+        if (!data) {
+            return next(new AppError('Gagal mendaftar akun pengguna', 500));
+        }
+
+        return res.status(201).json({
+            status: true,
+            message: 'Akun pengguna berhasil didaftarkan',
+            data
+        });
+    });
+
+    
     index = async (req, res) => {
         try {
             const data = await this._model.findAll();
             return res.json({
                 status: true,
-                message: "Data Ditemukan",
+                message: "Data Ditemukan semua",
                 total: data.length,
                 data
             })
@@ -24,24 +55,6 @@ class IndexController {
         }
     }
 
-    store  = async (req, res) => {
-        try {
-            const data = await this._model.create(req.body);
-            if(!data) { throw {message: "Gagal Daftar Akun Pengguna"}}
-
-            return res.json({
-                status: true,
-                message: "Data Berhasil Mendaftar Akun Pengguna",
-                data
-            })
-        } catch (error) {
-            return res.status(400).json({
-                status: false,
-                message: "Gagal Daftar Akun Pengguna",
-            })
-            
-        }
-    }
 }
 
-module.exports = IndexController;
+module.exports = SignupController;
